53943c6dbb4210315abea419db2d312426244ae1
fcyare06 <fcyare06@gmail.com>
2022-06-02 16:45:02 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(){
-puts("\"programing is like building a multilingual puzzle");
-return 0;
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
